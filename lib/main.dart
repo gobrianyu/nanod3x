f6b4fe3f589cdb24b46b25c78a1ba91b7bdccd5c
brian y. <brian127@uw.edu>
2025-03-13 19:06:47 -0700

    Setting up Firebase configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
-import 'dart:convert'; // For decoding JSON
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/services.dart'; // For loading the asset
+import 'package:flutter_dotenv/flutter_dotenv.dart';  // Add this import
 import 'package:pokellection/collection.dart';
 
 void main() async {
+  await dotenv.load(); // Load environment variables
+
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Load the Firebase configuration from the config.json file
-  final configString = await rootBundle.loadString('assets/config.json');
-  final configMap = jsonDecode(configString);
-
-  // Initialize Firebase with the loaded configuration
+  // Initialize Firebase with the loaded environment variables
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: configMap['apiKey'],
-      authDomain: configMap['authDomain'],
-      projectId: configMap['projectId'],
-      storageBucket: configMap['storageBucket'],
-      messagingSenderId: configMap['messagingSenderId'],
-      appId: configMap['appId'],
-      measurementId: configMap['measurementId'],
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID']!,
     ),
   );
 
