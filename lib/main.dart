a975c244ea8db44cccbb7519068d20e52ac1217a
brian y. <brian127@uw.edu>
2025-03-13 19:56:11 -0700

    Editing pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,43 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';  // Add this import
+import 'package:flutter_dotenv/flutter_dotenv.dart';  // Add this import for local development
 import 'package:pokellection/collection.dart';
 
 void main() async {
-  await dotenv.load(); // Load environment variables
-  print('help: ${dotenv.env['FIREBASE_API_KEY']}');
+  // Load environment variables only for non-production (debug/profile) mode
+  if (!kReleaseMode) {
+    await dotenv.load(); // Load .env for local or development builds
+    print('help: ${dotenv.env['FIREBASE_API_KEY']}');
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase with the loaded environment variables
+  // Initialize Firebase with environment variables or fallback to default for production
   await Firebase.initializeApp(
     options: FirebaseOptions(
-      apiKey: dotenv.env['FIREBASE_API_KEY']!,
-      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
-      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
-      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
-      appId: dotenv.env['FIREBASE_APP_ID']!,
-      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID']!,
+      apiKey: _getEnv('FIREBASE_API_KEY'),
+      authDomain: _getEnv('FIREBASE_AUTH_DOMAIN'),
+      projectId: _getEnv('FIREBASE_PROJECT_ID'),
+      storageBucket: _getEnv('FIREBASE_STORAGE_BUCKET'),
+      messagingSenderId: _getEnv('FIREBASE_MESSAGING_SENDER_ID'),
+      appId: _getEnv('FIREBASE_APP_ID'),
+      measurementId: _getEnv('FIREBASE_MEASUREMENT_ID'),
     ),
   );
 
   runApp(const MainApp());
+}
+
+// Helper function to get the value from environment variables or fall back to defaults
+String _getEnv(String key) {
+  if (!kReleaseMode) {
+    // Return from .env in non-production environments (like local or testing)
+    return dotenv.env[key] ?? 'default-$key';
+  } else {
+    // Return the environment variable set by GitHub Actions in production
+    return String.fromEnvironment(key, defaultValue: 'default-$key');
+  }
 }
 
 class MainApp extends StatelessWidget {
