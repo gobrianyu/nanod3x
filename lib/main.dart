105ef925f562bc85d73333bf1891191f20d14844
brian y. <brian127@uw.edu>
2025-03-13 18:41:23 -0700

    Removing failing files & fixing config

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
+import 'dart:convert'; // For decoding JSON
 import 'package:flutter/material.dart';
-import 'package:pokellection/collection.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart'; // For loading the asset
+import 'package:pokellection/collection.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  // Load the Firebase configuration from the config.json file
+  final configString = await rootBundle.loadString('assets/config.json');
+  final configMap = jsonDecode(configString);
+
+  // Initialize Firebase with the loaded configuration
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: configMap['apiKey'],
+      authDomain: configMap['authDomain'],
+      projectId: configMap['projectId'],
+      storageBucket: configMap['storageBucket'],
+      messagingSenderId: configMap['messagingSenderId'],
+      appId: configMap['appId'],
+      measurementId: configMap['measurementId'],
+    ),
+  );
+
   runApp(const MainApp());
 }
 
@@ -16,7 +35,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Collection()
+        body: Collection(),
       ),
     );
   }
