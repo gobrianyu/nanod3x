b63082e4025a66c1dac7bfe93c9b17f265a852a1
brian y. <brian127@uw.edu>
2025-03-13 17:42:38 -0700

    Attempting a bypass for index.html script on build

---
+++ scripts/post-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Restore the custom index.html after build
+cp path/to/custom_index.html path/to/build/web/index.html
---
+++ scripts/pre-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Backup the custom index.html
+cp path/to/custom_index.html path/to/build/web/index.html
