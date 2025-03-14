e560c1dbacdc13d444f6dfed4af5172868c1c86a
brian y. <brian127@uw.edu>
2025-03-13 18:00:59 -0700

    Removed duplicate script

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Restore the custom index.html after build
-cp /lib/index.html /build/web/index.html
+cp ./lib/index.html ./build/web/index.html
--- scripts/pre-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Backup the custom index.html
-cp /lib/index.html /build/web/index.html
