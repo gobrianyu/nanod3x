32e67378dc894e722497576edc40de579880a810
brian y. <brian127@uw.edu>
2025-03-13 20:12:53 -0700

    Giving up on secret Firebase keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,25 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';  // Add this import for local development
 import 'package:pokellection/collection.dart';
 
 void main() async {
-  // Load environment variables only for non-production (debug/profile) mode
-  if (!kReleaseMode) {
-    await dotenv.load(); // Load .env for local or development builds
-    print('help: ${dotenv.env['FIREBASE_API_KEY']}');
-  }
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Firebase with environment variables or fallback to default for production
+  // Initialize Firebase
   await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: _getEnv('FIREBASE_API_KEY'),
-      authDomain: _getEnv('FIREBASE_AUTH_DOMAIN'),
-      projectId: _getEnv('FIREBASE_PROJECT_ID'),
-      storageBucket: _getEnv('FIREBASE_STORAGE_BUCKET'),
-      messagingSenderId: _getEnv('FIREBASE_MESSAGING_SENDER_ID'),
-      appId: _getEnv('FIREBASE_APP_ID'),
-      measurementId: _getEnv('FIREBASE_MEASUREMENT_ID'),
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyBQT_xFSTrgADVpmrxAvunKFx_X-BHj7Ws',
+      authDomain: 'nanod3x.firebaseapp.com',
+      projectId: 'nanod3x',
+      storageBucket: 'nanod3x.firebasestorage.app',
+      messagingSenderId: '789914336089',
+      appId: '1:789914336089:web:1d246db33cefe50e0caa48',
+      measurementId: 'G-V635L0ZLLT',
     ),
   );
 
   runApp(const MainApp());
-}
-
-// Helper function to get the value from environment variables or fall back to defaults
-String _getEnv(String key) {
-  if (!kReleaseMode) {
-    // Return from .env in non-production environments (like local or testing)
-    return dotenv.env[key] ?? 'default-$key';
-  } else {
-    // Return the environment variable set by GitHub Actions in production
-    return String.fromEnvironment(key, defaultValue: 'default-$key');
-  }
 }
 
 class MainApp extends StatelessWidget {
